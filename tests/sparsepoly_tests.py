a8eb913a9c0f8a3c8bcb101d23a78a81c1cd0057
pogudingleb <pogudin.gleb@gmail.com>
2020-10-16 15:34:57 +0300

    Spoly tests updated

--- tests/sparsepoly_tests.py
+++ tests/sparsepoly_tests.py
@@ -6,14 +6,14 @@ from sympy import QQ
 sys.path.insert(0, "../")
 sys.path.insert(0, "./")
 import clue
-from clue import SparsePolynomial
+from sparse_polynomial import SparsePolynomial
 
 exprs = [
-    "a * (3 * a + b) - Rational(17, 2) * (a + b)**5 - 3 * c * b * (c - a)",
-    "(a + b + c**2)**5 - 3 * a + b * 17 * 19 * Rational(1, 2)"
+    "a * (3 * a + b) - 8.5 * (a + b)**5 - 3 * c * b * (c - a)",
+    "(a + b + c**2)**5 - 3 * a + b * 17 * 19 * 0.5"
 ]
 
 for e in exprs:
     parsed = sympy.parse_expr(e)
-    sp = SparsePolynomial.from_sympy_expr(parsed, ["a", "b", "c"], QQ)
+    sp = SparsePolynomial.from_string(e, ["a", "b", "c"])
     assert(sympy.simplify(parsed - sympy.parse_expr(str(sp))) == 0)
