c78e39832334897545d470f81c6f74c2c36aa0b5
pogudingleb <pogudin.gleb@gmail.com>
2020-11-11 18:27:09 +0300

    Update fceri_ji.py

--- examples/fceri_ji/fceri_ji.py
+++ examples/fceri_ji/fceri_ji.py
@@ -1,7 +1,3 @@
-# Model fceri_ji from Sneddon, M. W., Faeder, J. R., & Emonet, T. (2011). 
-# Efficient modeling, simulation and coarse-graining of biological complexity with NFsim.
-# Nature methods, 8(2), 177.
-
 import sys
 import time
 
