2f3be7600e8aec901d209a0b2e58f269bfa59659
Antonio Jimenez-Pastor <kethewal@gmail.com>
2021-10-14 11:47:29 +0200

    Polished the class of Nual Numbers
    Added also the division of nual numbers.

--- clue/nual.py
+++ clue/nual.py
@@ -5,7 +5,6 @@ r'''
     :class:`NualNumber`.
 '''
 
-
 class NualNumber:
     r'''
         Dual numbers can be defined as a special ring structure in vectors of 
@@ -67,10 +66,49 @@ class NualNumber:
             [-3, -1, 1, 3]
             >>> a * c
             [4, 11, 14, 17]
-            >>> a**10 == a
+            >>> a**10 == [1, 20, 30, 40]
             True
             >>> c**3
-            [64, 48, 32, 16]
+            [64, 144, 96, 48]
+            >>> b**2
+            [4, 12]
+
+        Elements in the ring of coefficients are naturally embedded into `n`-ual numbers.
+        This class also allow natural operations with these elements extending 
+        them by adding zeros on the other coordinates::
+
+            >>> a - 1
+            [0, 2, 3, 4]
+            >>> a - set([1])
+            Traceback (most recent call last):
+            ...
+            TypeError: bad operand type for unary -: 'set'
+            >>> a + set([1,2,3,4])
+            Traceback (most recent call last):
+            ...
+            TypeError: unsupported operand type(s) for +: 'int' and 'set'
+            >>> a + [1,2,3,4] == 2*a
+            True
+
+        `n`-ual numbers can often be inverted. In fact, the ring `R_n` described above
+        has the following two properties:
+
+        * All elements are units or zero divisors.
+        * Zero divisors are those elements with first coefficient equal to zero.
+
+        This class also includes the arithmetic for dividing by all the units in the ring::
+
+            >>> a = NualNumber([1,2,3])
+            >>> 1/a
+            [1.0, -2.0, -3.0]
+            >>> a * (1/a) == 1
+            True
+            >>> a/[3,2,1]
+            [0.3333333333333333, 0.4444444444444444, 0.8888888888888888]
+
+        Be aware that these divisions may not be exact computations. For example, divisions with
+        the Python type ``int`` lead to the use of ``float`` type output, which is not an exact domain
+        of computations.
     '''
     def __init__(self, coeffs):
         self.__size = len(coeffs)
@@ -84,6 +122,59 @@ class NualNumber:
     def coeffs(self):
         return self.__coeffs.copy()
 
+    def _to_nual(self, other):
+        r'''
+            Method to convert an element into a `n`-ual number of the same length as ``self``.
+
+            This method will be used in all arithmetic operations with :class:`NualNumber`
+            to convert the other input to the same type if possible and will follow these rules:
+
+            * If ``other`` can be set as input for :class:`NualNumber`, then we create the
+              corresponding element and check if the length are the same. A :class:`ValueError`
+              will be raised if the length does not coincide.
+            * Otherwise, we put ``other`` in a list of the same length as ``self`` and create 
+              the corresponding :class:`NualNumber`. The behavior of the arithmetic will now 
+              rely on the type of the data inside the :class:`NualNumber`.
+
+            Input:
+                ``other`` - object to be casted into the same type of NualNumber
+
+            Output:
+                A :class:`NualNumber` with the casted version of ``other`` with the same 
+                length as ``self``.
+
+            Examples::
+
+                >>> from clue.nual import *
+                >>> a = NualNumber([1,2,3])
+                >>> a._to_nual(2)
+                [2, 0, 0]
+                >>> a._to_nual(set([1]))
+                [{1}, 0, 0]
+                >>> a._to_nual([1])
+                Traceback (most recent call last):
+                ...
+                TypeError: The input [1] has incorrect length
+                >>> a._to_nual(NualNumber([4,5,6]))
+                [4, 5, 6]
+                >>> a._to_nual([1,4,2])
+                [1, 4, 2]
+                >>> a._to_nual(a) == a
+                True
+        '''
+        try:
+            other = NualNumber(other)
+        except TypeError:
+            ## The input is not iterable with integers, hence we create a list with
+            ## appropriate length for it
+            return NualNumber([other] + [0 for _ in range(self.size-1)])
+
+        ## If a NualNumber was created, we check the tpe is correct
+        if(other.size == self.size):
+            return other
+        else:
+            raise TypeError("The input %s has incorrect length" %other)
+
     # Sequence methods
     def __len__(self):
         return self.size
@@ -95,38 +186,32 @@ class NualNumber:
         return iter(self.coeffs)
 
     ## Comparison methods
-    def __eq__(self, other):
-        if(not isinstance(other, NualNumber)):
-            return False
-        # other is a NualNumber
-        if(self.size != other.size):
-            return False
+    def is_zero(self):
+        return all(c == 0 for c in self.coeffs)
 
-        return all(self[i] == other[i] for i in range(self.size))
+    def __eq__(self, other):
+        try:
+            return (self - other).is_zero()
+        except:
+            return False
 
     ## Arithmetic operations
     def __add__(self, other):
-        if(not isinstance(other, NualNumber)):
-            try:
-                other = NualNumber(other)
-            except:
-                return NotImplemented
-        # other is now a NualNumber
-        if(self.size != other.size):
-            return NotImplemented # impossible to compute with different sizes
+        try:
+            other = self._to_nual(other)
+        except TypeError:
+            return NotImplemented
 
+        # other is now a NualNumber with the same size
         return NualNumber([self[i]+other[i] for i in range(self.size)])
 
     def __mul__(self, other):
-        if(not isinstance(other, NualNumber)):
-            try:
-                other = NualNumber(other)
-            except:
-                return NotImplemented
-        # other is now a NualNumber
-        if(self.size != other.size):
-            return NotImplemented # impossible to compute with different sizes
+        try:
+            other = self._to_nual(other)
+        except TypeError:
+            return NotImplemented
 
+        # other is now a NualNumber with the same size
         return NualNumber([self[0]*other[0]] + [self[0]*other[i]+self[i]*other[0] for i in range(1, self.size)])
 
     def __neg__(self):
@@ -137,15 +222,31 @@ class NualNumber:
 
     def __sub__(self, other):
         try:
-            other = -other # getting the negation of other
-        except:
+            other = self._to_nual(other)
+        except TypeError:
             return NotImplemented
 
-        return self + other # using code for __add__
+        # other is now a NualNumber with the same size
+        return self + (-other) # using code for __add__
+
+    def __inv__(self):
+        if(self[0] == 0):            
+            raise ZeroDivisionError("%d-ual numbers with first term 0 are not invertible" %self.size)
+
+        return NualNumber([1/self[0]] + [-self[i]/self[0]**2 for i in range(1, self.size)])
+
+    def __truediv__(self, other):
+        try:
+            other = self._to_nual(other)
+        except TypeError:
+            return NotImplemented
+
+        # other is now a NualNumber with the same size
+        return self * (other.__inv__()) # using code for __mul__
 
     def __pow__(self, exp):
         com = self[0]**(exp-1)
-        return NualNumber([c*com for c in self.coeffs])
+        return NualNumber([self[0]*com] + [exp*self[i]*com for i in range(1,self.size)])
 
     def __radd__(self, other): return self.__add__(other)
 
@@ -153,11 +254,15 @@ class NualNumber:
 
     def __rmul__(self, other): return self.__mul__(other)
 
+    def __rtruediv__(self, other): return (self.__inv__()).__mul__(other)
+
     def __iadd__(self, other): return self.__add__(other)
 
-    def __isub__(self, other): return (self).__sub__(other)
+    def __isub__(self, other): return self.__sub__(other)
 
     def __imul__(self, other): return self.__mul__(other)
+
+    def __itruediv__(self, other): return self.__truediv__(other)
 
     ## Other magic methods
     def __str__(self):
