77b130ac3d6b6ef27c6ee178971f0151dd661c9d
Antonio Jimenez-Pastor <kethewal@gmail.com>
2021-11-16 11:54:05 +0100

    Added model 13 to test time
    Removed unnecessary line from model 23

--- tests/time/BIOMD0000000013.py
+++ tests/time/BIOMD0000000013.py
@@ -6,11 +6,10 @@ sys.path.insert(0, "./../../")
 
 from clue import FODESystem, SparsePolynomial
 
-system = FODESystem(file=f"BIOMD0000000023.ode", parser="sympy")
-obs = SparsePolynomial.from_string("Fru", system.variables)
+system = FODESystem(file=f"BIOMD0000000013.ode", parser="sympy")
+obs = SparsePolynomial.from_string("x_CO2", system.variables)
 
 start = time.time()
 lumped = system.lumping([obs], print_reduction=False, loglevel="ERROR")
 end = time.time()
-
 print(f"{end - start}")
