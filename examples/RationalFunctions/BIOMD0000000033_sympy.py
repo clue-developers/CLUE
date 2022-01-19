b6f6be23b16a32673a004e4c5dbc556498e48e56
Antonio Jimenez-Pastor <kethewal@gmail.com>
2022-01-19 19:30:55 +0100

    Updated examples to run BIO...33 with the sympy reader

---
+++ examples/RationalFunctions/BIOMD0000000033_sympy.py
@@ -0,0 +1,20 @@
+import sys
+import time
+
+sys.path.insert(0, "../")
+sys.path.insert(0, "./../../")
+
+from clue import FODESystem, SparsePolynomial
+
+start = time.time()
+system = FODESystem(file=f"BIOMD0000000033.ode", parser="sympy")
+print(f"Read the model in {time.time() - start}s")
+obs = SparsePolynomial.from_string("freeEGFReceptor", system.variables)
+
+start = time.time()
+lumped = system.lumping([obs], print_system=True, loglevel="INFO", method="auto_diff")
+end = time.time()
+
+print(f"The size of the original model is {system.size}")
+print(f"The size of the reduced model is {lumped.size}")
+print(f"Computation took {end - start} seconds")
