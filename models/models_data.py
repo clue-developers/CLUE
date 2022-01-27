f0d9dbe7b70abb9dff774255512fbb934af11b18
Antonio Jiménez-Pastor <kethewal@gmail.com>
2022-01-27 18:15:14 +0100

    Updated format of the models json

--- models/models_data.py
+++ models/models_data.py
@@ -1,33 +1,74 @@
 import json, os
 
+class Model:
+    @staticmethod
+    def process_range(range):
+        ## No range case
+        if(range in (None, "")):
+            return []
+
+        ## We first remove spaces
+        range = range.replace(" ", "")
+        separate_values = range.split(",")
+        values = []
+        for value in separate_values:
+            if(value.find("..") > 0):
+                limits = value.split("..")
+                values += [el for el in range(int(limits[0]), int(limits[1]))]
+            else:
+                values += [int(value)]
+        return list(set(values))
+
+    @staticmethod
+    def process_doi(doi):
+        if(doi.find("doi.org/") > 0):
+            return doi.split("doi.org/")[1]
+        return doi
+
+    def __init__(self, name, type, ref_title, ref_doi, range=None):
+        self.__name = name
+        self.__type = type
+        self.__range = Model.process_range(range)
+        self.__title = ref_title
+        self.__doi = Model.process_doi(ref_doi)
+
+    @property
+    def name(self): return self.__name
+    @property
+    def type(self): return self.__type
+    @property
+    def range(self): return self.__range
+    @property
+    def title(self): return self.__title
+    @property
+    def doi(self): return self.__doi
+
+    def path(self, range=None):
+        if(len(self.range) == 0):
+            return os.path.join(script_dir, f"{self.type}/{self.name}.ode")
+        elif(range in self.range):
+            return os.path.join(script_dir, f"{self.type}/{self.name}({range}).ode")
+        
+        raise IndexError("The index %d is not valid for the ranged model %s" %(range, self.name))
+
+
 script_dir = os.path.dirname(__file__) if __name__ != "__main__" else "./"
 with open(os.path.join(script_dir,'data.json')) as f:
     data = json.load(f)
+    models = {key : Model(key, **data[key]) for key in data}
     
-def model_path(name):
-    for model in data:
-        if(model['name'] == name):
-            return os.path.join(script_dir, f"{model['type']}/{name}.ode")
-    raise IndexError(f"The model {name} is not in the database")
-
-def model_doi(model):
-    doi = model['ref_doi']
-    if(doi.find("doi.org/") > 0):
-        return doi.split("doi.org/")[1]
-    return doi
-
 ## Script area
 if __name__ == "__main__":
     ## Reading and sorting the models
-    data.sort(key = lambda model : (model['type'].upper(), model['name'].upper()))
     by_types = {}
-    for model in data:
-        if(not model['type'] in by_types):
-            by_types[model['type']] = []
-        by_types[model['type']] += [model]
+    for model in models:
+        if(not model.type in by_types):
+            by_types[model.type] = []
+        by_types[model.type] += [model]
 
     ## We create a README.md file for each type
     for type in by_types:
+        by_types[type].sort(key=lambda m: m.name.upper())
         with open(os.path.join(script_dir,f"{type}/README.md"), "w") as readme:
             ## First part of the file
             header = (f"## {type.capitalize()} Models\n\n"
@@ -37,14 +78,12 @@ if __name__ == "__main__":
                             "Below are the references to the original papers for some of the models:\n")
             readme.write(header)
             for model in by_types[type]:
-                if(all(el in ("", None) for el in (model['ref_title'], model['ref_doi']))): # no reference
-                    readme.write(f"* {model['name']}: No reference\n")
-                elif(model['ref_title'] in ("", None)): # no title but doi
-                    doi = model_doi(model)
-                    readme.write(f"* {model['name']}: [{doi}](https://doi.org/{doi})\n")
+                if(all(el in ("", None) for el in (model.title, model.doi))): # no reference
+                    readme.write(f"* {model.name}: No reference\n")
+                elif(model.title in ("", None)): # no title but doi
+                    readme.write(f"* {model.name}: [{model.doi}](https://doi.org/{model.doi})\n")
                 elif(model['ref_doi'] in ("", None)): # no doi but title
-                    readme.write(f"* {model['name']}: {model['ref_title']}\n")
+                    readme.write(f"* {model.name}: {model.title}\n")
                 else: # both title and doi
-                    doi = model_doi(model)
-                    readme.write(f"* {model['name']}: [{model['ref_title']}](https://doi.org/{doi})\n")
+                    readme.write(f"* {model.name}: [{model.title}](https://doi.org/{model.doi})\n")
             readme.write("\n")
