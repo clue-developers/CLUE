4f0c6d3914ff1aad775e84394f651038badacc86
Antonio Jiménez-Pastor <kethewal@gmail.com>
2022-01-27 13:13:45 +0100

    Fixed the automated generation of READMEs

--- models/models_data.py
+++ models/models_data.py
@@ -1,7 +1,6 @@
 import json, os
 
-script_dir = os.path.dirname(__file__)
-
+script_dir = os.path.dirname(__file__) if __name__ != "__main__" else "./"
 with open(os.path.join(script_dir,'data.json')) as f:
     data = json.load(f)
     
@@ -13,14 +12,14 @@ def model_path(name):
 
 def model_doi(model):
     doi = model['ref_doi']
-    if(doi.contains("doi.org/")):
+    if(doi.find("doi.org/") > 0):
         return doi.split("doi.org/")[1]
     return doi
 
 ## Script area
 if __name__ == "__main__":
     ## Reading and sorting the models
-    data.sort(key = lambda model : (model['type'], model['name']))
+    data.sort(key = lambda model : (model['type'].upper(), model['name'].upper()))
     by_types = {}
     for model in data:
         if(not model['type'] in by_types):
@@ -29,11 +28,11 @@ if __name__ == "__main__":
 
     ## We create a README.md file for each type
     for type in by_types:
-        with open(os.path.join(script_dir,f"{type}/README.mf"), "w") as readme:
+        with open(os.path.join(script_dir,f"{type}/README.md"), "w") as readme:
             ## First part of the file
-            header = (f"## {type} Models\n\n"
+            header = (f"## {type.capitalize()} Models\n\n"
                             "This folder contains a sequence of `.ode` files with different models from the [BioModels]"
-                            f"(https://www.ebi.ac.uk/biomodels/) database whose dynamics are of type {type}."
+                            f"(https://www.ebi.ac.uk/biomodels/) database whose dynamics are of type `{type}`. "
                             "The name of each model is either given by the theme of the model or the name in the database.\n\n"
                             "Below are the references to the original papers for some of the models:\n")
             readme.write(header)
@@ -42,10 +41,10 @@ if __name__ == "__main__":
                     readme.write(f"* {model['name']}: No reference\n")
                 elif(model['ref_title'] in ("", None)): # no title but doi
                     doi = model_doi(model)
-                    readme.write(f"* {model['name']}: [{doi}](https://www.doi.org/{doi})\n")
+                    readme.write(f"* {model['name']}: [{doi}](https://doi.org/{doi})\n")
                 elif(model['ref_doi'] in ("", None)): # no doi but title
                     readme.write(f"* {model['name']}: {model['ref_title']}\n")
                 else: # both title and doi
                     doi = model_doi(model)
-                    readme.write(f"* {model['name']}: [{model['ref_title']}](https://www.doi.org/{doi})\n")
+                    readme.write(f"* {model['name']}: [{model['ref_title']}](https://doi.org/{doi})\n")
             readme.write("\n")
--- models/rational/README.md
+++ models/rational/README.md
@@ -1,16 +1,15 @@
 ## Rational Models
 
-This folder contains a sequence of `.ode` files with different models from the [BioModels](https://www.ebi.ac.uk/biomodels/) database
-whose dynamics are rational functions. The name of each model is either given by the theme of the model or the name in the database.
+This folder contains a sequence of `.ode` files with different models from the [BioModels](https://www.ebi.ac.uk/biomodels/) database whose dynamics are of type `rational`. The name of each model is either given by the theme of the model or the name in the database.
 
 Below are the references to the original papers for some of the models:
 * BIOMD0000000013: [Applications of metabolic modelling to plant metabolism](https://doi.org/10.1093/jxb/erh090)
-* BIOMD0000000023: [Analysis of sucrose accumulation in the sugar cane culm on the basis of in vitro kinetic data](https://doi.org/10.1042/bj3580437)
-* BIOMD0000000033: [The statistical mechanics of complex signaling networks: nerve growth factor signaling](https://doi.org/10.1088/1478-3967/1/3/006)
-* BIOMD0000000113: [Protein phosphorylation driven by intracellular calcium oscillations: A kinetic analysis](https://doi.org/10.1016/0301-4622(92)80018-z)
+* BIOMD0000000023: [Analysis of sucrose accumulation in the sugar cane culm on the basis of in vitro kinetic data](https://doi.org/10.1088/1478-3967/1/3/006)
+* BIOMD0000000033: [Protein phosphorylation driven by intracellular calcium oscillations: A kinetic analysis](https://doi.org/10.1016/0301-4622(92)80018-z)
 * BIOMD0000000182: [Cell shape and negative links in regulatory motifs together control spatial information flow in signaling networks](https://doi.org/10.1016/j.cell.2008.04.025)
 * BIOMD0000000313: [Dynamic mathematical modeling of IL13-induced signaling in hodgkin and primary mediastinal b-cell lymphoma allows prediction of therapeutic targets](https://doi.org/10.1158/0008-5472.can-10-2987)
 * BIOMD0000000448: [Insulin signaling in type 2 diabetes](https://doi.org/10.1074/jbc.m112.432062)
 * BIOMD0000000526: [Intra- and interdimeric caspase-8 self-cleavage controls strength and timing of CD95-induced apoptosis](https://doi.org/10.1126/scisignal.2004738)
 * BIOMD0000000535: [A multiscale model of interleukin-6-mediated immune regulation in crohn's disease and its application in drug discovery and development](https://doi.org/10.1038/psp.2013.64)
 * MODEL1502270000: [Mechanistic links between cellular trade-offs, gene expression, and growth](https://doi.org/10.1073/pnas.1416533112)
+
