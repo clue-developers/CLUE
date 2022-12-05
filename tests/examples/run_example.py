3d487a6125a28f0ce6c5032288cdfae92ad38023
Antonio Jimenez-Pastor <kethewal@gmail.com>
2022-12-05 15:18:59 +0100

    Add profiling to the runner of examples

--- tests/examples/run_example.py
+++ tests/examples/run_example.py
@@ -1,6 +1,9 @@
-import signal, sys, time
+import signal, sys, time, pstats
 
 sys.path.insert(0, "./../../") # clue is here
+
+from contextlib import nullcontext
+from cProfile import Profile
 
 from clue import FODESystem, SparsePolynomial
 from examples_data import get_example #pylint: disable=import-error
@@ -25,6 +28,7 @@ if __name__ == "__main__":
     observables = example.observables
     timeout = 0
     output = f"result_{example.name}.example.txt"
+    profile = None
 
     ## Checking the rest of the arguments
     n = 2
@@ -37,6 +41,8 @@ if __name__ == "__main__":
             timeout = int(args[n+1])
         elif(args[n] in ("-o", "-output", "--o", "--output")):
             output = int(args[n+1])
+        elif(args[n] in ("-p", "-profile", "--profile", "--p")):
+            profile = f"./profiles/result_{example.name}.profile.txt"
 
     ## Creating the file in case it is needed
     if(output == "stdout"):
@@ -46,34 +52,43 @@ if __name__ == "__main__":
     else:
         file = open(output, "w")
 
+    ## Starting profiler if there is any
+    with Profile() if profile else nullcontext() as pr:
     ## Setting up the handler for the signal
-    old_handler = signal.signal(signal.SIGALRM, alarm_handler)
+        old_handler = signal.signal(signal.SIGALRM, alarm_handler)
 
-    ## now we can run the model properly
-    system = FODESystem(file=example.path_model(), parser=read)
+        ## now we can run the model properly
+        system = FODESystem(file=example.path_model(), parser=read)
 
-    for obs_set in observables:
-        print("===============================================", file=file)
-        obs_polys = [SparsePolynomial.from_string(s, system.variables) for s in obs_set]
+        for obs_set in observables:
+            print("===============================================", file=file)
+            obs_polys = [SparsePolynomial.from_string(s, system.variables) for s in obs_set]
 
-        lumped = None
-        signal.alarm(timeout)
-        try:
-            start = time.time()
-            lumped = system.lumping(obs_polys, method=matrix, file=file)
-            end = time.time()
-        except TimeoutError:
-            pass
-        signal.alarm(0)
+            lumped = None
+            signal.alarm(timeout)
+            try:
+                start = time.time()
+                lumped = system.lumping(obs_polys, method=matrix, file=file)
+                end = time.time()
+            except TimeoutError:
+                pass
+            signal.alarm(0)
+            
+            if(not lumped == None):
+                print(f"The size of the original model is {system.size}", file=file)
+                print(f"The size of the reduced model is {lumped.size}", file=file)
+                print(f"Computation took {end - start} seconds", file=file)
+            else:
+                print(f"The example could not finish in the given timeout ({timeout}", file=file)
         
-        if(not lumped == None):
-            print(f"The size of the original model is {system.size}", file=file)
-            print(f"The size of the reduced model is {lumped.size}", file=file)
-            print(f"Computation took {end - start} seconds", file=file)
-        else:
-            print(f"The example could not finish in the given timeout ({timeout}", file=file)
+        ## Reverting changes
+        signal.signal(signal.SIGALRM, old_handler)
     
-    ## Reverting changes
-    signal.signal(signal.SIGALRM, old_handler)
+    # Closing the output file (if opened)
     if(not output in ("stdout", "stderr")):
         file.close()
+    # Saving the profile (if set)
+    if profile:
+        stats = pstats.Stats(pr)
+        stats.sort_stats(pstats.SortKey.TIME)
+        stats.dump_stats(filename=profile)
