9a2d021b18004b9b923ffe4e131ccd006132848e
Antonio Jimenez-Pastor <kethewal@gmail.com>
2023-02-16 12:56:45 +0100

    Remake of run_example
    Added context manager for timeouts
    Added more timeouts for reading and building matrices

--- tests/examples/run_example.py
+++ tests/examples/run_example.py
@@ -1,4 +1,4 @@
-import os, pickle, pstats, signal, sys, time
+import os, pstats, signal, sys, time
 
 SCRIPT_DIR = os.path.dirname(__file__) if __name__ != "__main__" else "./"
 sys.path.insert(0, os.path.join(SCRIPT_DIR, "..", "..")) # models and clue is here
@@ -9,11 +9,24 @@ from cProfile import Profile
 from clue import FODESystem, SparsePolynomial, Subspace, OrthogonalSubspace, UncertainFODESystem, UncertainLDESystem
 from examples_data import get_example #pylint: disable=import-error
 
-def alarm_handler(sgn, _):
-    if(sgn == signal.SIGALRM):
-        raise TimeoutError
-    else:
-        raise RuntimeError
+class Timeout(object):
+    def __init__(self, seconds):
+        self.seconds = seconds
+        self.old = None
+    def __enter__(self):
+        self.old = signal.signal(signal.SIGALRM, Timeout.alarm_handler)
+        signal.alarm(self.seconds)
+        return self
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
+        signal.signal(signal.SIGALRM, self.old)
+
+    @staticmethod
+    def alarm_handler(sgn, _):
+        if(sgn == signal.SIGALRM):
+            raise TimeoutError
+        else:
+            raise RuntimeError
 
 if __name__ == "__main__":
     ## Managing the input
@@ -62,26 +75,53 @@ if __name__ == "__main__":
     ## Starting profiler if there is any
     with Profile() if profile else nullcontext() as pr:
     ## Setting up the handler for the signal
-        first_time = time.time()
-        old_handler = signal.signal(signal.SIGALRM, alarm_handler)
+        total_time = time.time()
 
         print(f"[run_example] Reading example {example.name} ({read=})...", flush=True)
 
-        read_time = time.time()
-        ## now we can run the model properly
-        if read == "uncertain":
-            system = FODESystem(file=example.path_model(), parser="polynomial", lumping_subspace=subs_class)
-            system = UncertainFODESystem.from_FODESystem(system, example.delta, type=example.unc_type)
-        else:
-            system = FODESystem(file=example.path_model(), parser=read, lumping_subspace=subs_class)
-        read_time = time.time() - read_time
-
+        try: #Reading the file
+            with Timeout(timeout):
+                read_time = time.time()
+                ## now we can run the model properly
+                if read == "uncertain":
+                    system = FODESystem(file=example.path_model(), parser="polynomial", lumping_subspace=subs_class)
+                    system = UncertainFODESystem.from_FODESystem(system, example.delta, type=example.unc_type)
+                else:
+                    system = FODESystem(file=example.path_model(), parser=read, lumping_subspace=subs_class)
+                read_time = time.time() - read_time
+        except TimeoutError:
+            print(f"[run_example] Timeout ({timeout}) on {example.name} ({read=})", flush=True)
+            print(f"Timeout error detected: {timeout}", file=file)
+            print(f"Timeout while reading the .ode file", file=file)
+            # Closing the output file (if opened)
+            if(not output in ("stdout", "stderr")):
+                file.close()
+            # Saving the profile (if set)
+            if profile:
+                stats = pstats.Stats(pr)
+                stats.sort_stats(pstats.SortKey.TIME)
+                stats.dump_stats(filename=profile)
+            sys.exit(0)
         
         print(f"[run_example] Building matrices for {example.name} ({matrix=})...", flush=True)
-        matrices_time = time.time()
-        system.construct_matrices(matrix)
-        matrices_time = time.time() - matrices_time
-
+        try: #Building the matrices for lumping
+            with Timeout(timeout):
+                matrices_time = time.time()
+                system.construct_matrices(matrix)
+                matrices_time = time.time() - matrices_time
+        except TimeoutError:
+            print(f"[run_example] Timeout ({timeout}) on {example.name} ({matrix=})", flush=True)
+            print(f"Timeout error detected: {timeout}", file=file)
+            print(f"Timeout while building the matrices", file=file)
+            # Closing the output file (if opened)
+            if(not output in ("stdout", "stderr")):
+                file.close()
+            # Saving the profile (if set)
+            if profile:
+                stats = pstats.Stats(pr)
+                stats.sort_stats(pstats.SortKey.TIME)
+                stats.dump_stats(filename=profile)
+            sys.exit(0)
         print(f"[run_example] Running example {example.name} ({len(observables)} cases)...", flush=True)
         for obs_set in observables:
             print(f"[run_example]     ++ {example.name} (({observables.index(obs_set)+1}/{len(observables)}))", flush=True)
@@ -90,11 +130,11 @@ if __name__ == "__main__":
             obs_polys = [SparsePolynomial.from_string(s, system.variables, system.field) for s in obs_set]
 
             lumped = None
-            signal.alarm(timeout)
             try:
-                start = time.time()
-                lumped = system.lumping(obs_polys, method=matrix, file=file)
-                end = time.time()
+                with Timeout(timeout):
+                    lumping_time = time.time()
+                    lumped = system.lumping(obs_polys, method=matrix, file=file)
+                    lumping_time = time.time() - lumping_time
             except TimeoutError:
                 print(f"Timeout error detected: {timeout}", file=file)
                 print("###############################################", file=file)
@@ -103,14 +143,13 @@ if __name__ == "__main__":
                 print("Overflow error detected", file=file)
                 print("###############################################", file=file)
                 continue
-            signal.alarm(0)
             
             if(not lumped == None):
                 obs_str = str(tuple(obs_polys))
                 lumped.save(example.out_path(read, matrix, "too long" if len(obs_str) > 100 else tuple(obs_polys)), format="clue")
                 print(f"The size of the original model is {lumped.old_system.size}", file=file)
                 print(f"The size of the reduced model is {lumped.size}", file=file)
-                print(f"Computation took {end - start} seconds", file=file)
+                print(f"Computation took {lumping_time} seconds", file=file)
                 print(f"PROPERTIES OF THE LUMPING:::", file=file)
                 print(f"Is the lumping unweighted?: {lumped.is_unweighted()}", file=file)
                 print(f"Is the lumping positive?: {lumped.is_positive()}", file=file)
@@ -129,13 +168,12 @@ if __name__ == "__main__":
         print(f"[run_example] ## Finished example {example.name} ##", flush=True)
         
         ## Reverting changes
-        signal.signal(signal.SIGALRM, old_handler)
-        final_time = time.time()
+        total_time = time.time() - total_time
         print("===============================================", file=file)
         print("== END OF EXAMPLES", file=file)
         print(f"Time for reading the model: {read_time}", file=file)
         print(f"Time for building matrices: {matrices_time}", file=file)
-        print(f"Total time in execution: {final_time - first_time}", file=file)
+        print(f"Total time in execution: {total_time}", file=file)
 
     
     # Closing the output file (if opened)
