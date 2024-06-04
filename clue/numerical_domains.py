581db7f3ad9cbd8b6eb38afcf8aa466d79bd5dc8
Antonio Jiménez-Pastor <kethewal@gmail.com>
2024-06-04 11:45:23 +0200

    ♻️  Fixed numerical_domains   * Added method __contains__ with some tests.   * ADded method __repr__

--- clue/numerical_domains.py
+++ clue/numerical_domains.py
@@ -28,6 +28,61 @@ class NumericalField:
     def zero(self):
         return self.__dtype(0)
     
+    def __contains__(self, element):
+        r'''
+            Method to check whether an element can be seen as an element of the numerical field.
+
+            This method tries to convert the element to the :class:`NumericalField` and compares 
+            the result. If the two elements are equal, then we consider the element is in the field.
+
+            To guarantee the proper behavior of other functions, we recommend to still use the method
+            :func:`convert` before operating with ``element``.
+
+            Input:
+
+            - ``element``: the element to be checked.
+
+            Output:
+
+            ``True`` if the given ``element`` can be seen as an element of ``self``.
+
+            Examples::
+
+                >>> from clue.numerical_domains import RR, CC
+                >>> from sympy import QQ
+                >>> 1 in RR # integers can be reals
+                True
+                >>> 1.0 in RR # floats are reals
+                True
+                >>> 1j in RR # complex are not reals
+                False
+                >>> 1+0j in RR # we do not check specifically the imaginary part
+                False
+                >>> 
+                >>> 1 in CC # integers can be reals
+                True
+                >>> 1.0 in CC # floats are reals
+                True
+                >>> 1j in CC # complex are not reals
+                True
+
+            However, we ca not distinguish elements that are not from primitive types of Python
+            even when they are technically part of the field::
+
+                >>> half = QQ(1,2)
+                >>> half in RR
+                False
+                >>> QQ.evalf(half) in RR
+                True
+        '''
+        try:
+            return self.convert(element) == element
+        except Exception:
+            return False
+    
+    def __repr__(self):
+        return f"NumField[{self.__dtype}]"
+    
 RR = NumericalField(float64)
 RR16 = NumericalField(float16)
 RR32 = NumericalField(float32)
